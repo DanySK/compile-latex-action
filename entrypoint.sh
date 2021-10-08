a48e9c87a9b78fa8c49d519fc5e1da9ec1636e98
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-08 09:46:15 +0200

    Add debug prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
+echo "Exporting GEM_HOME, setting it to $(ruby -e 'puts Gem.user_dir')"
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+echo "Adding GEM_HOME/bin to PATH"
 export PATH="$PATH:$GEM_HOME/bin"
+echo "New PATH: $PATH"
+echo "Launching the tex scanning and compilation proces..."
 /entrypoint.rb
