4780d4d86b23c8044a373aee06947d9823cd944c
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-09 09:55:38 +0200

    Update test.rb

--- test.rb
+++ test.rb
@@ -8,14 +8,14 @@ my_latex = [
     'Template-ACM-Article',
     'Template-Elsevier-Article',
     'Template-Elsevier-CAS-DC',
-    'Template-IEEE-Computer-Society-Magazines'
-    'Template-IEEE-Conference-Proceedings'
-    'Template-LaTeX-achemso'
-    'Template-LaTeX-CI'
-    'Template-LaTeX-ERC'
-    'Template-LaTeX-LMCS'
-    'Template-LaTeX-LNCS'
-    'Template-LaTeX-MDPI'
+    'Template-IEEE-Computer-Society-Magazines',
+    'Template-IEEE-Conference-Proceedings',
+    'Template-LaTeX-achemso',
+    'Template-LaTeX-CI',
+    'Template-LaTeX-ERC',
+    'Template-LaTeX-LMCS',
+    'Template-LaTeX-LNCS',
+    'Template-LaTeX-MDPI',
     'Template-PhD-Tesi-Giovanni-Ciatto',
 ]
 
