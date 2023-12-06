912486a7b54a119f0b309bafd55759fd84759726
Danilo Pianini <danilo.pianini@gmail.com>
2023-12-06 08:40:29 +0100

    ci: enable semantic-release

---
+++ release.config.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+var publishCmd = `
+docker push --all-tags \${process.env.IMAGE_NAME}:\${nextRelease.version}
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
